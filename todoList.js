488db4e8634350eb2817f6c77f1767d10a1dd3ea
kathy95917 <kathy95917@naver.com>
2020-06-14 22:23:22 +0900

    Add TodoList function

---
+++ todoList.js
@@ -0,0 +1,60 @@
+const ul = document.querySelector(".todoList"),
+  toDoForm = document.querySelector(".toDoForm"),
+  toDoInput = toDoForm.querySelector("input");
+
+let toDoArr = [];
+function handleSubmit(e) {
+  e.preventDefault();
+  const currentVal = toDoInput.value;
+  showToDo(currentVal);
+  toDoInput.value = "";
+}
+
+function deleteToDo(e) {
+  const btn = e.target;
+  const li = btn.parentNode;
+  ul.removeChild(li);
+  const removeToDos = toDoArr.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDoArr = removeToDos;
+  localStorage.setItem("todo", JSON.stringify(toDoArr));
+}
+
+function showToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  const span = document.createElement("span");
+  const newId = toDoArr.length + 1;
+  delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo);
+  span.innerText = text;
+  li.appendChild(delBtn);
+  li.appendChild(span);
+  li.id = newId;
+  console.log(li);
+  ul.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+  toDoArr.push(toDoObj);
+  localStorage.setItem("todo", JSON.stringify(toDoArr));
+}
+
+function checkLocalStorage() {
+  const list = localStorage.getItem("todo");
+  if (list !== null) {
+    const parseList = JSON.parse(list);
+    parseList.forEach(function (toDo) {
+      showToDo(toDo.text);
+    });
+  }
+}
+
+function init() {
+  checkLocalStorage();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
