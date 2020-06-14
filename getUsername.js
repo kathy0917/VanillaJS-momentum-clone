7466d9467c41dcc961aeee22525693b692c608f3
kathy95917 <kathy95917@naver.com>
2020-06-14 19:17:22 +0900

    Add getUsername function

--- getUsername.js
+++ getUsername.js
@@ -1,7 +1,31 @@
-function getUsername() {}
+const input = document.querySelector(".inputName"),
+  form = document.querySelector(".form"),
+  username = document.querySelector(".username");
 
+function showUsername(name) {
+  username.innerText = `Hello! ${name}`;
+}
+
+function AskUsername() {
+  form.addEventListener("submit", getUsername);
+}
+
+function getUsername(e) {
+  e.preventDefault();
+  let inputValue = input.value;
+  showUsername(inputValue);
+  localStorage.setItem("name", inputValue);
+}
+function checkLocalStorage() {
+  const currentName = localStorage.getItem("name");
+  if (currentName !== null) {
+    showUsername(currentName);
+  } else {
+    AskUsername();
+  }
+}
 function init() {
-  getUsername();
+  checkLocalStorage();
 }
 
 init();
